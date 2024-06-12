bf8e365d8e220c015e90f0bbf9d9413055e170b4
gusagomez <64170659+gusagomez@users.noreply.github.com>
2024-06-12 19:30:34 -0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace PruebaApi
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment iwebhost)
+        {
+            app.Build();
+            //app.Equals();
+            app.GetHashCode();
+            app.GetType();
+            app.New();
+            
+
+           
+        }
+        public IConfiguration? configuration { get; }
+        public Startup(IConfiguration configuracion)
+        {
+
+        }
+        public void ConfigureServices(IServiceCollection coleccion)
+        {
+            coleccion.AddControllers();
+            coleccion.AddSwaggerGen();
+            coleccion.AddEndpointsApiExplorer();
+
+        }
+
+
+    }
+}
